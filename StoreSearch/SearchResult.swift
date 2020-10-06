1c3e97c0503e79a11d6531574677f06922fb142f
Lorenzo Masucci <lormas91@gmail.com>
2020-10-06 13:26:54 +0100

    Add a tableView and a searchBar. The results of the search bar are fake for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Lorenzo on 06/10/2020.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
