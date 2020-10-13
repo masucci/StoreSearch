38c325b7eec9d53b2322e30155eb142b727d98e5
Lorenzo Masucci <lormas91@gmail.com>
2020-10-13 09:38:08 +0100

    Add fetching data from web service using synchronous network request

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -7,7 +7,93 @@
 
 import Foundation
 
-class SearchResult {
-    var name = ""
-    var artistName = ""
+class ResultArray: Codable {
+
+    var resultCount = 0
+    var results = [SearchResult]()
+}
+
+class SearchResult: Codable, CustomStringConvertible {
+
+    var artistName: String? = ""
+    var trackName: String? = ""
+    var kind: String? = ""
+    var trackPrice: Double? = 0.0
+    var currency = ""
+
+    var trackViewUrl: String?
+    var collectionName: String?
+    var collectionViewUrl: String?
+    var collectionPrice: Double?
+    var itemPrice: Double?
+    var itemGenre: String?
+    var bookGenre: [String]?
+
+    var imageSmall = ""
+    var imageLarge = ""
+
+
+
+    enum CodingKeys: String, CodingKey {
+        case imageSmall = "artworkUrl60"
+        case imageLarge = "artworkUrl100"
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genres"
+        case itemPrice = "price"
+        case kind, artistName, currency
+        case trackPrice, trackName, trackViewUrl
+        case collectionName, collectionViewUrl, collectionPrice
+    }
+    
+    static func < (lhs:SearchResult, rhs: SearchResult) -> Bool {
+        return lhs.name.localizedStandardCompare(rhs.name) == .orderedAscending
+    }
+
+    var description: String {
+        return "Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")"
+    }
+
+
+    var name: String {
+        return trackName ?? collectionName ?? ""
+    }
+
+    var storeURL: String {
+        return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+
+    var price: Double {
+        return trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+    
+    var type: String {
+        let kind = self.kind ?? "audiobook"
+        switch kind {
+        case "album": return "Album"
+        case "audiobook": return "Audio Book"
+        case "book": return "Book"
+        case "ebook": return "E-Book"
+        case "feature-movie": return "Movie"
+        case "music-video": return "Music Video"
+        case "podcast": return "Podcast"
+        case "software": return "App"
+        case "song": return "Song"
+        case "tv-episode": return " TV Episode"
+        default: break
+        }
+        return "Unknown"
+    }
+    
+    var artist: String {
+        return artistName ?? ""
+    }
+
+    var genre: String {
+        if let genre = itemGenre {
+            return genre
+        } else if let genres = bookGenre {
+            return genres.joined(separator: ", ")
+        }
+        return ""
+    }
 }
